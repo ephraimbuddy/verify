5a5b640dabe338fcdb609b789cf9b0b11503082f
EphraimBuddy <splendidzigy24@gmail.com>
2019-08-11 18:24:00 +0100

    initial commit

---
+++ verifyphone/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+
+class TimestampedModel(models.Model):
+    # A timestamp representing when this object was created.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # A timestamp reprensenting when this object was last updated.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+        ordering = ['-created_at', '-updated_at']
+
+
+
+class PhoneNumber(TimestampedModel):
+    
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    number = models.CharField(max_length=17, blank=True) 
+    verified = models.BooleanField(default=False)
+    verification_date = models.DateField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.user.email
+
+
